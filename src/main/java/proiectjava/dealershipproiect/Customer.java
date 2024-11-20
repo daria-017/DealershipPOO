14033d6d0bbb1da5e45077490ef88e1f8d3e385a
aa437 <aa437@WIN-3.CTI.UGAL.RO>
2024-11-20 21:29:02 +0200

    Completed Customer class!

--- src/main/java/proiectjava/dealershipproiect/Customer.java
+++ src/main/java/proiectjava/dealershipproiect/Customer.java
@@ -13,6 +13,28 @@ public class Customer {
     String surname;
     int phoneNumber;
     ArrayList<Vehicle> vehicles;
+
+    public Customer() {
+        this("", "", 0, null);
+    }
+    
+    public Customer(String forename, String surname, int phoneNumber, ArrayList<Vehicle> vehicles) {
+        this.forename = forename;
+        this.surname = surname;
+        this.phoneNumber = phoneNumber;
+        this.vehicles = vehicles;
+    }
+    
+    public Customer(Customer customer) {
+        this.forename = customer.forename;
+        this.surname = customer.surname;
+        this.phoneNumber = customer.phoneNumber;
+        this.vehicles = customer.vehicles;
+    }
+    
+    public void addVehicle(Vehicle vehicle) {
+        this.vehicles.add(vehicle);
+    }
     
     public String getForename() {
         return forename;
@@ -44,6 +66,11 @@ public class Customer {
 
     public void setVehicles(ArrayList<Vehicle> vehicles) {
         this.vehicles = vehicles;
+    }
+    
+    @Override
+    public String toString() {
+        return "Customer{" + "forename=" + this.forename + ", surname=" + this.surname + ", phoneNumber=" + this.phoneNumber + ", vehicles=" + this.vehicles + '}';
     }
  
     
