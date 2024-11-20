a2d47fdd77c8bf71582f7d589fe29380721e77ed
Daria Rata <rata.daria1999@gmail.com>
2024-11-20 20:12:18 +0200

    First Commit!

---
+++ src/main/java/proiectjava/dealershipproiect/DealershipProiect.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package proiectjava.dealershipproiect;
+
+/**
+ *
+ * @author Daria
+ */
+public class DealershipProiect {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
