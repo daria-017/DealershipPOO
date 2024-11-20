07612906f5e284b15a1a0bb98b51d5c9cc6cedf2
Daria Rata <rata.daria1999@gmail.com>
2024-11-20 22:41:42 +0200

    First Commit!

--- src/main/java/proiectjava/dealershipproiect/JetPlane.java
+++ src/main/java/proiectjava/dealershipproiect/JetPlane.java
@@ -4,6 +4,9 @@
  */
 package proiectjava.dealershipproiect;
 
+import java.awt.Color;
+import javax.swing.ImageIcon;
+
 /**
  *
  * @author Daria
@@ -35,130 +38,127 @@ public class JetPlane extends Vehicle {
         this.highSpeed = false;
         this.propulsionSystem = "unknown";
 
-        //this.engine = null;
-        //this.transmission = null;
-    }
-    //?
+   
+    }    
     // Constructor cu toți parametrii
-//    public JetPlane(int id, int year, int price, int weight, String brand, String model, String color,
-//                    /*Transmission transmission, Engine engine*/ int maxAltitude, int maxRange, int maxSpeed,
-//                    boolean militaryAircraft, boolean luggageCapacity, String propulsionSystem, boolean highSpeed,
-//                    int engineCycles) {
-//        super(id, year, price, weight, brand, model, color, /*transmission, engine*/ photo);
-//        this.maxAltitude = maxAltitude;
-//        this.maxRange = maxRange;
-//        this.maxSpeed = maxSpeed;
-//        this.militaryAircraft = militaryAircraft;
-//        this.luggageCapacity = luggageCapacity;
-//        this.propulsionSystem = propulsionSystem;
-//        this.highSpeed = highSpeed;
-//        this.engineCycles = engineCycles;
-//       // this.engine = engine;
-//       // this.transmission = transmission;
-//    }
+    public JetPlane(int id, String brand, String model, int year, Color color, int price, int weight,
+                    Engine engine, Transmission transmission, ImageIcon photo, int maxAltitude, int maxRange, int maxSpeed,
+                    boolean militaryAircraft, boolean luggageCapacity, String propulsionSystem, boolean highSpeed,
+                    int engineCycles) {
+        super(id, brand, model, year, color, price, weight, engine, transmission, photo);
+        this.maxAltitude = maxAltitude;
+        this.maxRange = maxRange;
+        this.maxSpeed = maxSpeed;
+        this.militaryAircraft = militaryAircraft;
+        this.luggageCapacity = luggageCapacity;
+        this.propulsionSystem = propulsionSystem;
+        this.highSpeed = highSpeed;
+        this.engineCycles = engineCycles;
+       // this.engine = engine;
+       // this.transmission = transmission;
+    }
 
-//    // Constructor de copiere
-//    public JetPlane(JetPlane jetplane) {
-//        super(jetplane.id, jetplane.year, jetplane.price, jetplane.weight, jetplane.brand, jetplane.model, jetplane.color,
-//              /*jetplane.transmission, jetplane.engine,*/ jetplane.photo);
-//        this.maxAltitude = jetplane.maxAltitude;
-//        this.maxRange = jetplane.maxRange;
-//        this.maxSpeed = jetplane.maxSpeed;
-//        this.militaryAircraft = jetplane.militaryAircraft;
-//        this.luggageCapacity = jetplane.luggageCapacity;
-//        this.propulsionSystem = jetplane.propulsionSystem;
-//        this.highSpeed = jetplane.highSpeed;
-//        this.engineCycles = jetplane.engineCycles;
-//        //this.engine = jetplane.engine;
-//        //this.transmission = jetplane.transmission;
-//    }
-//
-//    // Getters și Setters
-//    public int getMaxAltitude() {
-//        return maxAltitude;
-//    }
-//
-//    public void setMaxAltitude(int maxAltitude) {
-//        this.maxAltitude = maxAltitude;
-//    }
-//
-//    public int getMaxRange() {
-//        return maxRange;
-//    }
-//
-//    public void setMaxRange(int maxRange) {
-//        this.maxRange = maxRange;
-//    }
-//
-//    public int getMaxSpeed() {
-//        return maxSpeed;
-//    }
-//
-//    public void setMaxSpeed(int maxSpeed) {
-//        this.maxSpeed = maxSpeed;
-//    }
-//
-//    public boolean isMilitaryAircraft() {
-//        return militaryAircraft;
-//    }
-//
-//    public void setMilitaryAircraft(boolean militaryAircraft) {
-//        this.militaryAircraft = militaryAircraft;
-//    }
-//
-//    public boolean hasLuggageCapacity() {
-//        return luggageCapacity;
-//    }
-//
-//    public void setLuggageCapacity(boolean luggageCapacity) {
-//        this.luggageCapacity = luggageCapacity;
-//    }
-//
-//    public String getPropulsionSystem() {
-//        return propulsionSystem;
-//    }
-//
-//    public void setPropulsionSystem(String propulsionSystem) {
-//        this.propulsionSystem = propulsionSystem;
-//    }
-//
-//    public boolean isHighSpeed() {
-//        return highSpeed;
-//    }
-//
-//    public void setHighSpeed(boolean highSpeed) {
-//        this.highSpeed = highSpeed;
-//    }
-//
-//    public int getEngineCycles() {
-//        return engineCycles;
-//    }
-//
-//    public void setEngineCycles(int engineCycles) {
-//        this.engineCycles = engineCycles;
-//    }
-//
-//    public Engine getEngine() {
-//        return engine;
-//    }
-//
-//    public void setEngine(Engine engine) {
-//        this.engine = engine;
-//    }
-//
-//    public Transmission getTransmission() {
-//        return transmission;
-//    }
-//
-//    public void setTransmission(Transmission transmission) {
-//        this.transmission = transmission;
-//    }
-//
-//    // Suprascrierea metodei toString()
-//    @Override
-//    public String toString() {
-//        return 
-//    }
-// */
+    // Constructor de copiere
+    public JetPlane(JetPlane jetplane) {
+        super(jetplane);
+        this.maxAltitude = jetplane.maxAltitude;
+        this.maxRange = jetplane.maxRange;
+        this.maxSpeed = jetplane.maxSpeed;
+        this.militaryAircraft = jetplane.militaryAircraft;
+        this.luggageCapacity = jetplane.luggageCapacity;
+        this.propulsionSystem = jetplane.propulsionSystem;
+        this.highSpeed = jetplane.highSpeed;
+        this.engineCycles = jetplane.engineCycles;
+
+    }
+
+    // Getters și Setters
+    public int getMaxAltitude() {
+        return maxAltitude;
+    }
+
+    public void setMaxAltitude(int maxAltitude) {
+        this.maxAltitude = maxAltitude;
+    }
+
+    public int getMaxRange() {
+        return maxRange;
+    }
+
+    public void setMaxRange(int maxRange) {
+        this.maxRange = maxRange;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public boolean isMilitaryAircraft() {
+        return militaryAircraft;
+    }
+
+    public void setMilitaryAircraft(boolean militaryAircraft) {
+        this.militaryAircraft = militaryAircraft;
+    }
+
+    public boolean hasLuggageCapacity() {
+        return luggageCapacity;
+    }
+
+    public void setLuggageCapacity(boolean luggageCapacity) {
+        this.luggageCapacity = luggageCapacity;
+    }
+
+    public String getPropulsionSystem() {
+        return propulsionSystem;
+    }
+
+    public void setPropulsionSystem(String propulsionSystem) {
+        this.propulsionSystem = propulsionSystem;
+    }
+
+    public boolean isHighSpeed() {
+        return highSpeed;
+    }
+
+    public void setHighSpeed(boolean highSpeed) {
+        this.highSpeed = highSpeed;
+    }
+
+    public int getEngineCycles() {
+        return engineCycles;
+    }
+
+    public void setEngineCycles(int engineCycles) {
+        this.engineCycles = engineCycles;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public Transmission getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(Transmission transmission) {
+        this.transmission = transmission;
+    }
+
+    // Suprascrierea metodei toString()
+    @Override
+     public String toString() {
+        return "JetPlane "+super.toString()+"max altitude: "+maxAltitude+" max range: "+maxRange+" max speed: "+maxSpeed+" engine cycles: "+engineCycles+
+                "military aircraft:"+militaryAircraft+" luggage capacity: "+luggageCapacity+" high speed: "+highSpeed+" propulsion system: "+propulsionSystem;
+    }
+
 }
 
