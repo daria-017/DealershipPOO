926e8065193a0756a319094321d046f68242793f
Daria Rata <rata.daria1999@gmail.com>
2024-11-21 07:59:47 +0200

    First Commit!

--- src/main/java/proiectjava/dealershipproiect/Vehicle.java
+++ src/main/java/proiectjava/dealershipproiect/Vehicle.java
@@ -10,7 +10,7 @@ package proiectjava.dealershipproiect;
  */
 import javax.swing.ImageIcon;
 import java.awt.Color;
-public class Vehicle {
+abstract class Vehicle {
     int id;
     String brand;
     String model;
@@ -141,7 +141,7 @@ public class Vehicle {
     }
     
     public String toString(){
-        return " id"+id+" brand: "+brand+" model: "+model+" year: "+" color: "+color+" price: "+price+" weight(in kg): "+weight+"engine={" + (engine != null ? engine.toString() : "No engine") + "}" +
+        return " id: "+id+" brand: "+brand+" model: "+model+" year: "+" color: "+color+" price: "+price+" weight(in kg): "+weight+"engine={" + (engine != null ? engine.toString() : "No engine") + "}" +
            " transmission={" + (transmission != null ? transmission.toString() : "No transmission");
     }
     
