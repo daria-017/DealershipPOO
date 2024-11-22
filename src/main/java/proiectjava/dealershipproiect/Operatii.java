3f55f3610c56f9afb198bd8d789aab18d8d11b7a
Daria Rata <rata.daria1999@gmail.com>
2024-11-22 17:01:05 +0200

    First Commit!

---
+++ src/main/java/proiectjava/dealershipproiect/Operatii.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proiectjava.dealershipproiect;
+
+/**
+ *
+ * @author Daria
+ */
+abstract class Operatii {
+    public abstract String schimbUlei();
+    public abstract String schimbAnvelope();
+}
